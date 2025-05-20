cd85266a195b9855eef86ef01f9fb8d8d4ef6fa9
Lucas Vieira <lucasvieira@protonmail.com>
2025-05-20 01:07:29 -0300

    Remove PSX debug target from .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,13 +5,5 @@
 		  (neotreebuf (seq-filter (lambda (buf) (equal (buffer-name buf) " *NeoTree*"))
 					  (buffer-list))))
 	      (setq-local flycheck-clang-include-path includes)
-	      (setq-local flycheck-gcc-include-path includes)
-	      (dap-register-debug-template
-	       "PSX Debug"
-	       (list :name "PSX -- Engine debug"
-		     :type "gdbserver"
-		     :request "attach"
-		     :gdbpath "/usr/bin/gdb-multiarch"
-		     :target ":3333"
-		     :cwd root)))))))
+	      (setq-local flycheck-gcc-include-path includes))))))
 
